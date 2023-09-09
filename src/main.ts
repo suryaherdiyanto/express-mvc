9054a603cd3e111256a24ac2f7ca2b17eb9a0eb0
Surya <suryaherdiyanto@gmail.com>
2023-09-09 18:46:36 +0800

    configure body parser & express json for pattern route

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import { Response } from "express";
+import Express, { Response } from "express";
 import * as dotenv from "dotenv";
 import session, {SessionOptions} from "express-session";
 import FileStore from "session-file-store";
 
 
 const app = express();
-const fileStorage = FileStore(session);
+dotenv.config();
+
 const secureCookie = (process.env.NODE_ENV === 'production') ? true:false;
+const fileStorage = FileStore(session);
+console.log(process.env.APP_KEY);
 const sessionOptions: SessionOptions = {
 	secret: process.env.APP_KEY as string,
 	store: new fileStorage(),
@@ -16,16 +19,13 @@ const sessionOptions: SessionOptions = {
 	cookie: { secure: secureCookie }
 };
 
-dotenv.config();
 app.use(session(sessionOptions));
+app.use('^/api/*', Express.json());
+app.use(/^\/(?!api).*/, Express.urlencoded({ extended: false }));
 
 app.get('/', (_, res: Response) => {
-	res.send('Hello World');
-});
-
-app.get('/test', (_, res: Response) => {
-	res.send('From test');
-});
+	res.send('Woooo');
+})
 
 app.listen(3000, () => {
 	console.log('Server started at port 3000');
