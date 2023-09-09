18ec546846c82d78fa42dbaeeefe5ac6a9a3bcdf
Surya <suryaherdiyanto@gmail.com>
2023-09-09 18:05:22 +0800

    setup session & file session storage

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { Response } from "express";
 import * as dotenv from "dotenv";
+import session from "express-session";
+import FileStore from "session-file-store";
 
 
 const app = express();
+const fileStorage = FileStore(session);
+
 dotenv.config();
+app.use(session({
+	secret: process.env.APP_KEY as string,
+	store: new fileStorage(),
+	resave: false,
+	saveUninitialized: false
+}));
 
 app.get('/', (_, res: Response) => {
 	res.send('Hello World');
