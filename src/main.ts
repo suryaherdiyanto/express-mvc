bc26aaed4b4d2daf0f762065d132c24e0781d536
Surya <suryaherdiyanto@gmail.com>
2023-09-09 17:36:08 +0800

    configure .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { Response } from "express";
+import * as dotenv from "dotenv";
 
 
 const app = express();
-
+dotenv.config();
 
 app.get('/', (_, res: Response) => {
 	res.send('Hello World');
@@ -12,7 +13,6 @@ app.get('/', (_, res: Response) => {
 app.get('/test', (_, res: Response) => {
 	res.send('From test');
 });
-
 
 app.listen(3000, () => {
 	console.log('Server started at port 3000');
