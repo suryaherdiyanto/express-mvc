678a605f84d57a33678a3bbd2a6595fc41efa295
Surya <suryaherdiyanto@gmail.com>
2023-09-09 18:10:23 +0800

    configure secure cookie

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import express from "express";
 import { Response } from "express";
 import * as dotenv from "dotenv";
-import session from "express-session";
+import session, {SessionOptions} from "express-session";
 import FileStore from "session-file-store";
 
 
 const app = express();
 const fileStorage = FileStore(session);
-
-dotenv.config();
-app.use(session({
+const secureCookie = (process.env.NODE_ENV === 'production') ? true:false;
+const sessionOptions: SessionOptions = {
 	secret: process.env.APP_KEY as string,
 	store: new fileStorage(),
 	resave: false,
-	saveUninitialized: false
-}));
+	saveUninitialized: false,
+	cookie: { secure: secureCookie }
+};
+
+dotenv.config();
+app.use(session(sessionOptions));
 
 app.get('/', (_, res: Response) => {
 	res.send('Hello World');
