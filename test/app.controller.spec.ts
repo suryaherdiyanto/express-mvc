222e19501016c363b643d0ae2eb575146a87d628
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-09-16 13:46:56 +0800

    setup unit test

---
+++ test/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect, jest, beforeEach } from "@jest/globals";
+import { Request, Response } from "express";
+import { home } from "../app.controller";
+
+
+let request: Partial<Request>;
+let response: Partial<Response>;
+beforeEach(() => {
+    response = {
+        render: jest.fn()
+    };
+});
+
+describe("Home Controller", () => {
+    it("Should return home view", () => {
+
+        const res = jest.spyOn(response, 'render');
+
+        home(request as Request, response as Response);
+        expect(res).toHaveBeenCalledWith('index');
+    });
+})
