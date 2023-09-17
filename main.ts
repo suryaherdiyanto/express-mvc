eaa2f566abecc502e8890e8403ac6bdbb32613f3
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-09-17 13:01:17 +0800

    register providers

--- main.ts
+++ main.ts
@@ -1,22 +1,19 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import "./providers";
+
 import {handle404, handle500} from "./middlewares/error-handle.middleware";
 import session from "express-session";
 import FileStore from "session-file-store";
 import { App } from "./app";
-import { home } from "./modules/app/app.controller";
-import { AppService } from "./modules/app/app.service";
 
 const fileStorage = FileStore(session);
 
 export const app = new App({
 	sessionStorage: new fileStorage(),
 	statefull: true
-}).registerService(AppService.name, new AppService()).getApp();
-
-
-app.get('/', home);
+}).getApp();
 
 app.use(handle404);
 app.use(handle500);
--- providers.ts
+++ providers.ts
@@ -1,1 +1,8 @@
 import "reflect-metadata";
+import { container } from "tsyringe";
+import { AppService } from "./modules/app/app.service";
+import { AppController } from "./modules/app/app.controller";
+
+
+container.resolve(AppService);
+container.resolve(AppController);
