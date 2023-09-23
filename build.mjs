637660d847a1076ee9c8a97bca291e48ccbe12ea
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-09-23 14:20:25 +0800

    adjust build.mjs for production

--- build.mjs
+++ build.mjs
@@ -8,18 +8,25 @@ if (process.env.NODE_ENV === 'production') {
   assetPath = `assets/js/client.${version}.js`;
 }
 
-const build = await esbuild.context({
+const buildOption = {
     entryPoints: ["./assets/js/client.js"],
     outfile: `./public/${assetPath}`,
     bundle: true,
     minify: process.env.NODE_ENV === 'production' ? true:false,
     sourcemap: process.env.NODE_ENV !== 'production' ? true:false,
-});
+};
 
 if (process.env.NODE_ENV !== 'production') {
-  build.watch();
-  console.log('Build watch!');
+  const build = await esbuild.context(buildOption);
+
+  if (process.env.NODE_ENV !== 'production') {
+    build.watch();
+    console.log('Build watch!');
+  }
+} else {
+  esbuild.buildSync(buildOption);
 }
+
 
 fs.writeFileSync('./public/build-manifest.json', JSON.stringify({
   "/assets/js/client.js": assetPath
