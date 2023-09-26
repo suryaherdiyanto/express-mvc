0c32100b15dc285cbe760aa01311fe843aa7f566
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-09-26 21:06:58 +0800

    adjust init app

--- main.ts
+++ main.ts
@@ -8,16 +8,18 @@ import session from "express-session";
 import FileStore from "session-file-store";
 import { App } from "./app";
 import { appRoute } from "./modules/app/app.route";
+import { Application } from "express";
 
 const fileStorage = FileStore(session);
 
-export const app = new App({
-	sessionStorage: new fileStorage(),
-	statefull: true,
-	staticPath: './dist/client'
-}).getApp();
 
 async function createViteServer() {
+	const app = new App({
+		sessionStorage: new fileStorage(),
+		statefull: true,
+		staticPath: './dist/client'
+	}).getApp();
+
 	const { createServer } = await import("vite");
 	const server = await createServer({ server: { middlewareMode: true }, appType: 'custom'});
 	app.use(server.middlewares);
@@ -25,10 +27,14 @@ async function createViteServer() {
 	return app;
 }
 
-createViteServer().then(() => {
+const initApp = (app: Application) => {
 	app.use(appRoute);
 	app.use(handle404);
 	app.use(handle500);
 
 	app.listen(3000, () => console.log('App started at port 3000'));
-});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+	createViteServer().then(initApp);
+}
