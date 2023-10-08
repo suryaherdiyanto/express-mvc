12cce6182b9215587d780ce8ad61bbb0ec140a6d
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-08 15:29:19 +0800

    add renderInertia middleware

--- middlewares/vue.middleware.ts
+++ middlewares/vue.middleware.ts
@@ -1,9 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 
+interface InertiaPage {
+    component: string,
+    props: {
+        [key: string]: unknown
+    },
+    url: string,
+    version: string
+}
 export const useInertia = (req: Request, res: Response, next: NextFunction) => {
 
     res.renderInertia = (component: string, props: {}) => {
-        res.render(component, { props: JSON.stringify(props) });
+        const page: InertiaPage = {
+            component,
+            props,
+            url: req.path,
+            version: process.env.APP_VERSION as string,
+        }
+
+        if (req.headers['X-Inertia'] || req.headers['x-inertia']) {
+            return res.type('application/json').json(page);
+        }
+
+        res.type('html').render('index', { props: JSON.stringify(page) });
     }
     next();
 }
